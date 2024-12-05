aec7d0be7cba16b0c27b5d9d99bcbe715ec5b181
Fortune101-ai <Sensefortune01@gmail.com>
2024-12-05 14:13:21 +0200

    wrap up sales and home cards

--- src/main.js
+++ src/main.js
@@ -34,3 +34,26 @@ scrollRevealOptions().reveal(".header_btns",{
     ...scrollRevealOptions,delay:1500
 })
 
+scrollRevealOptions().reveal(".arrival_card", {
+    ...scrollRevealOptions, interval: 500
+})
+
+scrollRevealOptions().reveal(".sale_image img", {
+    ...scrollRevealOptions, origin: "left"
+})
+
+scrollRevealOptions().reveal(".sale_content h2", {
+    ...scrollRevealOptions, delay: 500
+})
+scrollRevealOptions().reveal(".sale_content p", {
+    ...scrollRevealOptions, delay: 1000
+})
+scrollRevealOptions().reveal(".sale_content h4", {
+    ...scrollRevealOptions, delay: 1000
+})
+scrollRevealOptions().reveal(".sale_btn", {
+    ...scrollRevealOptions, delay: 1500
+})
+
+
+
