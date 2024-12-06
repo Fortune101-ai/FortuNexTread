4ffffc27dacde4c68938d21bf477936afb1bac92
Fortune101-ai <Sensefortune01@gmail.com>
2024-12-06 12:51:28 +0200

    add images, update links

--- src/main.js
+++ src/main.js
@@ -20,42 +20,42 @@ const scrollRevealOptions = {
     duration: 1000,
 }
 
-scrollRevealOptions().reveal(".header_image img", {
+ScrollReveal().reveal(".header_image img", {
     ...scrollRevealOptions, origin: "right"
 })
 
-scrollRevealOptions().reveal(".header_content h1",{
+ScrollReveal().reveal(".header_content h1",{
     ...scrollRevealOptions,delay:500
 })
-scrollRevealOptions().reveal(".header_content p",{
+ScrollReveal().reveal(".header_content p",{
     ...scrollRevealOptions,delay:1000
 })
-scrollRevealOptions().reveal(".header_btns",{
+ScrollReveal().reveal(".header_btns",{
     ...scrollRevealOptions,delay:1500
 })
 
-scrollRevealOptions().reveal(".arrival_card", {
+ScrollReveal().reveal(".arrival_card", {
     ...scrollRevealOptions, interval: 500
 })
 
-scrollRevealOptions().reveal(".sale_image img", {
+ScrollReveal().reveal(".sale_image img", {
     ...scrollRevealOptions, origin: "left"
 })
 
-scrollRevealOptions().reveal(".sale_content h2", {
+ScrollReveal().reveal(".sale_content h2", {
     ...scrollRevealOptions, delay: 500
 })
-scrollRevealOptions().reveal(".sale_content p", {
+ScrollReveal().reveal(".sale_content p", {
     ...scrollRevealOptions, delay: 1000
 })
-scrollRevealOptions().reveal(".sale_content h4", {
+ScrollReveal().reveal(".sale_content h4", {
     ...scrollRevealOptions, delay: 1000
 })
-scrollRevealOptions().reveal(".sale_btn", {
+ScrollReveal().reveal(".sale_btn", {
     ...scrollRevealOptions, delay: 1500
 })
 
-scrollRevealOptions().reveal(".favourite_card", {
+ScrollReveal().reveal(".favourite_card", {
     ...scrollRevealOptions, interval: 500
 })
 
